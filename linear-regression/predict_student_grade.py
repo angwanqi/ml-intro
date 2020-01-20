c8bd5e421821a965b0f1667aeb25c91938737597
angwanqi <42666676+angwanqi@users.noreply.github.com>
2020-01-20 21:53:54 +0000

    Update predict_student_grade.py

--- linear-regression/predict_student_grade.py
+++ linear-regression/predict_student_grade.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
 
 # Read in student data from file
 data = pd.read_csv("student-mat.csv", sep=";")
